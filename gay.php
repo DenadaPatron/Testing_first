5ea6a4b8f0a780831f73290415282002289125e2
1408229 <artukras@gmail.com>
2016-10-03 16:46:27 +0100

    suicide

--- gay.php
+++ gay.php
@@ -4,17 +4,22 @@
     <body>
         <p>
             <?php
-            $myage = 19;
-            if($myage >= 16){
-                print "You are allowed to buy specs!";
-            }
-            elseif($myage >= 18) {
-                print "You are allowed to buy mugs!";
-            }
-            elseif($myage >= 21 ) {
-                 print "you are allowed to buy sausage rolls" ;
-                }
 
+            $wantedgood = "mugs";
+
+            switch ($wantedgood) {
+                case "specs":
+                    echo "You have to be over 16 to buy specs";
+                    break;
+                case "mugs":
+                    echo "You have to be over 18 to buy mugs";
+                    break;
+                case "sausage rolls":
+                    echo "You have to be over 21 to buy sausage rolls";
+                    break;
+                default:
+                    echo "please specify a right product";
+            }
             ?>
          </p>
         </body>
