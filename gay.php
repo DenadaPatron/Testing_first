660bc5e1f3ffb4d03924c509b620fb4ed4f5954c
1408229 <artukras@gmail.com>
2016-10-17 15:46:43 +0100

    suicide

--- gay.php
+++ gay.php
@@ -21,38 +21,8 @@
 
 
               data("Arthur", 5, 7, 3);
-
-
           }
 
-
-
-
-
-
-
-
-
-
-
-           /* Now	that	the	specs,	mugs,	and	sausage	roll	lottery	has	occurred,	life	in	Aberdeen	has	started	to	turn	back	to
-normal.	However,	the	clean-up	operation	associated	with	the	abuse	of	these	items	must	now	begin.
-            Write	a	php	function	that	takes	in:
-• A	persons	name
-• The	number	of	specs	they	own
-• The	number	of	mugs	they	own
-• The	Number	of	sausage	rolls	they	own
-The	function	must	then	print	out	a	‘wanted’	banner	similar	to	below:
-Wanted: Joe Bloggs
-Known to be in possession of the following items:
-Specs (x)
-Mugs (x)
-Sausage Rolls (x)
-Award for capture: £xx
-Use	the	following	equation	to	work	out	the	award	for	capture.
-����� ��� ������� = 10 ∗ (���������� ∗ ��������� ∗ �����������������);
-2
-Call	3	instances	of	this	method	to	produce	wanted	banners	for	people*/
 
             ?>
         </p>
