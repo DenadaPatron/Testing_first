10a79cd4da70fe0cf102738bdd8a54622796ff93
1408229 <artukras@gmail.com>
2016-10-17 15:48:37 +0100

    suicide

--- gay.php
+++ gay.php
@@ -3,13 +3,28 @@
         <head></head>
     <body>
     <p>
-            <?php
+        <?php
+
+        function data($name, $specsno, $sausagesno, $mugsno){
+            echo "Wanted: " . $name . "<br/> \n";
+            echo "Known to be in possession of the following items: " . "<br/> \n";
+            echo "The number of  mugs they own:(" . $mugsno . ")" . "<br/> \n";
+            echo "The number of specs they own:(" .$specsno. ")" . "<br/> \n";
+            echo "The number of sausage rolls they own :(" . $sausagesno . ")" . "<br/ \n>";
 
 
-         echo "hi";
+            $aCapt = 10* ((($specsno*$mugsno*$sausagesno)^2)/2);
 
 
-            ?>
-        </p>
-        </body>
+            echo "Award for the capture: £" . $aCapt;
+
+
+            data("Arthur", 5, 7, 3);
+        }
+
+
+
+        ?>
+    </p>
+    </body>
 </html>
