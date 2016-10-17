91782e19e8b36a768cf88468c0ca1a2d3e1ef895
1408229 <artukras@gmail.com>
2016-10-17 15:30:34 +0100

    suicide

--- gay.php
+++ gay.php
@@ -2,25 +2,59 @@
     <html>
         <head></head>
     <body>
-        <p>
+
             <?php
 
-            $wantedgood = "specs";
 
-            switch ($wantedgood) {
-                case "specs":
-                    echo "You have to be over 16 to buy specs";
-                    break;
-                case "mugs":
-                    echo "You have to be over 18 to buy mugs";
-                    break;
-                case "sausage rolls":
-                    echo "You have to be over 21 to buy sausage rolls";
-                    break;
-                default:
-                    echo "please specify a right product";
-            }
+          function data($name, $specsno, $sausagesno, $mugsno){
+              echo "Wanted: " . $name . "<br/> \n";
+              echo "Known to be in possession of the following items: " . "<br/> \n";
+              echo "The number of  mugs they own:(" . $mugsno . ")" . "<br/> \n";
+              echo "The number of specs they own:(" .$specsno. ")" . "<br/> \n";
+              echo "The number of sausage rolls they own :(" . $sausagesno . ")" . "<br/ \n>";
+
+
+              $aCapt = 10* ((($specsno*$mugsno*$sausagesno)^2)/2);
+
+
+              echo "Award for the capture: £" . $aCapt;
+
+
+
+
+
+
+          }
+
+
+
+
+
+
+
+
+
+
+
+           /* Now	that	the	specs,	mugs,	and	sausage	roll	lottery	has	occurred,	life	in	Aberdeen	has	started	to	turn	back	to
+normal.	However,	the	clean-up	operation	associated	with	the	abuse	of	these	items	must	now	begin.
+            Write	a	php	function	that	takes	in:
+• A	persons	name
+• The	number	of	specs	they	own
+• The	number	of	mugs	they	own
+• The	Number	of	sausage	rolls	they	own
+The	function	must	then	print	out	a	‘wanted’	banner	similar	to	below:
+Wanted: Joe Bloggs
+Known to be in possession of the following items:
+Specs (x)
+Mugs (x)
+Sausage Rolls (x)
+Award for capture: £xx
+Use	the	following	equation	to	work	out	the	award	for	capture.
+����� ��� ������� = 10 ∗ (���������� ∗ ��������� ∗ �����������������);
+2
+Call	3	instances	of	this	method	to	produce	wanted	banners	for	people*/
             ?>
-         </p>
+
         </body>
 </html>
