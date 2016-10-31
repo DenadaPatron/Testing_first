f0915d965249ad2f8bdeb959718f548e6fd4d94f
1408229 <artukras@gmail.com>
2016-10-31 16:12:25 +0000

    html change

--- week3/dtb_pages/xmenfilms.php
+++ week3/dtb_pages/xmenfilms.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: 1408229
- * Date: 24/10/2016
- * Time: 16:43
- */
+
+include('../dbconnect.php');
+$sql = 'SELECT * FROM marvelmovies WHERE title like "X-men%"';
+
+$result=myslqi_query($link, $sql);
+while($row=$result->fetch_array()){
+    $movieTitle=$row['title'];
+    echo "<p>" .$movieTitle . "</p>";
+}
+
+/*include('../dbconnect.php');
+
+$sql =  'SELECT * FROM marvelmovies';
+
+$result=mysqli_query($link, $sql);
+while($row=$result->fetch_array())
+{
+$movieTitle=$row['title'];
+echo "<p>" . $movieTitle . "</p>";
+
+}*/
+
+
+?>
