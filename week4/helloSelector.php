231d93f161d677d053de47eea9955d3f240d4cf0
1408229 <artukras@gmail.com>
2016-11-21 13:22:22 +0000

    week 4beggining

--- week4/helloSelector.php
+++ week4/helloSelector.php
@@ -1,8 +1,8 @@
 <?php
 
-echo "Where would you like to say hello\n";
+echo "Where would you like to say hello";
 
-echo '<a href="helloPrinter.php?query=earth">Earth</a><br/>';
+echo '<br/><a href="helloPrinter.php?query=earth">Earth</a><br/>';
 echo '<a href="helloPrinter.php?query=mars">Mars</a><br/>';
 echo '<a href=helloPrinter.php?query=uranus">Uranus</a><br/>';
 
