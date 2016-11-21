53fccf855f82adad45de10e7219bcdc18318d67f
1408229 <artukras@gmail.com>
2016-11-21 14:50:33 +0000

    week 4beggining

--- week4/formResponse.php
+++ week4/formResponse.php
@@ -1,2 +1,10 @@
 <?php
+
+$forename = $_POST["forename"];
+$surname = $_POST["surname"];
+$gender = $_POST["gender"];
+$power = $_POST["power"];
+
+echo $forename . $surname . $gender . $power;
+
 ?>
