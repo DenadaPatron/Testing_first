07042a96fd3198f10fb2d555fcd8554d038df5c2
DenadaPatron <artukras@gmail.com>
2016-12-15 17:09:35 +0000

    newsite bootstrap tutorial

--- Master/createClub.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("../includes/db_connection.php"); 
-
-$_POST[""];
-
-$stmt = "INSERT INTO Clubs (UserID, photoName, description, PhotoLink, price) VALUE ('$id', '$photoName', '$description', '$link', '$price')";
-
-
-?>
